d22c4fffaead81d54e88db61b059c86ee64b984d
Ariel Ibarra <arielibarra39@gmail.com>
2020-09-12 21:56:46 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -10,7 +10,7 @@ end
 def reverse_each_word(string)
   array = string.split(" ") 
   test_array = []
-  array.collect do|string| #iterate over the array
+  array.collect do|string| 
     test_array << string.reverse #reverse each word in the array
   end
   test_array.join(" ")
