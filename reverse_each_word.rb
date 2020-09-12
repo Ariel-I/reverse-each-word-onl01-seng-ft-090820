00d894fa03cf525ee9dd54ca589100f1d2caf55d
Ariel Ibarra <arielibarra39@gmail.com>
2020-09-12 22:05:02 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,7 +2,7 @@ def reverse_each_word(string)
   array = string.split("")
   new_array = []
   array.each do |string|
-    new_array << string.reveserse
+    new_array << string.reverse
   end
   new_array.join(" ")
 end
