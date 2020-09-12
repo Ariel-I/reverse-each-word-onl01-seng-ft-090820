6a3f4c32978aca285d6b74f596aae4e1444b7466
Ariel Ibarra <arielibarra39@gmail.com>
2020-09-12 22:09:34 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,17 +1,17 @@
 def reverse_each_word(string)
   original_array = string.split("")
-  new_array = []
+  return_array = []
   array.each do |string|
-    new_array << string.reverse
+    return_array << string.reverse
   end
-  new_array.join("")
+  return_array.join("")
 end
 
 def reverse_each_word_with_collect(string)
-  second_array = string.split("")
-  new_new_array = []
-  second_array.collect do |string|
-    new_new_array << string.reverse 
+  array = string.split("")
+  test_array = []
+  array.collect do |string|
+    test_array << string.reverse 
   end
-  new_new_array.join("")
+  test_array.join("")
 end
