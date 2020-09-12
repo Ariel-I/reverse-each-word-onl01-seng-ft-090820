d45086d4d6f7476790cb46e43667cb23b9e5987f
Ariel Ibarra <arielibarra39@gmail.com>
2020-09-12 22:17:50 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
 def reverse_each_word_each(string)
   first_array = string.split(" ")
   return_array = []
-  original_array.each do|string|
+  first_array.each do|string|
     return_array << string.reverse
   end
   return_array.join(" ")
